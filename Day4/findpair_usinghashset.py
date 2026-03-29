380babe44bd7f5e95550cbb60525fadb603162f2
Ankitha <ankitha07142@gmail.com>
2026-03-29 14:44:27 +0530

    Delete Day4 directory

--- Day4/findpair_usinghashset.py
+++
@@ -1,9 +0,0 @@
-arr = [7, 2, 15, 5, 11]
-target = 18
-seen = set()
-for num in arr:    
-    complement = target - num    
-    if complement in seen:
-        print("Pair found:", complement, num)
-        break    
-    seen.add(num)
--- Day4/reversearr_usingtwopointer.py
+++
@@ -1,8 +0,0 @@
-arr = [86,78,36,28,14]
-left = 0
-right = len(arr) - 1
-while left < right:
-    arr[left], arr[right] = arr[right], arr[left]   
-    left += 1
-    right -= 1
-print("Reversed array:", arr)
