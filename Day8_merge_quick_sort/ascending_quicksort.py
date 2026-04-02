1f91e37f8d8d20fde71bd83aa633892cce64b16a
Ankitha <ankitha07142@gmail.com>
2026-04-02 22:43:20 +0530

    Day8: Mergesort and Quicksort

---
+++ Day8_merge_quick_sort/ascending_quicksort.py
@@ -0,0 +1,18 @@
+def quick_sort(arr):
+    if len(arr)<=1:
+        return arr
+    pivot=arr[len(arr)//2]
+    left=[]
+    middle=[]
+    right=[]
+    for x in arr:
+        if x<pivot:
+            left.append(x)
+        elif x>pivot:
+            right.append(x)
+        else:
+            middle.append(x)
+    return quick_sort(left) + middle + quick_sort(right)
+arr=[66,34,78,91,26,48]
+sorted_array=quick_sort(arr)
+print("Sorted array:",sorted_array)
