a97e00572fbca5c8a4ec6f184b00c3867abda2f7
Ankitha <ankitha07142@gmail.com>
2026-04-12 00:03:04 +0530

    Day15 : Nect Greater Element using Stack

--- Day15_NextGreaterElement_usingstack/next_greater_element.py
+++ Day15_NextGreaterElement_usingstack/next_greater_element.py
@@ -1,8 +1,9 @@
 arr=[9,6,7,13,11,5]
 stack=[]
 for num in arr:
-    while stack and num> stack[-1]:
-        stack.append(num)
+    while stack and num > stack[-1]:
+        print(stack.pop(), "-->", num)
+    stack.append(num)
 while stack:
     print(stack.pop(),"-->",-1)
 
